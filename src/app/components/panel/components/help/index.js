271ba5c42d3cbb5c030933ec7e008d5df9d4600d
8th713 <8th713@gmail.com>
2014-10-01 18:55:06 +0900

    feat(help): リンクを追加

--- src/app/components/panel/components/help/index.js
+++ src/app/components/panel/components/help/index.js
@@ -13,6 +13,8 @@ module.exports = {
   created: function created() {
     this.name = GM_info.script.name;
     this.version = GM_info.script.version;
+    this.homepage = GM_info.script.homepage;
+    this.support = GM_info.script.supportURL;
   },
   attached: function attached() {
     this.map = keys.getMap();
