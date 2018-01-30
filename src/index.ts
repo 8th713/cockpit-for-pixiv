bb10cf0b344ba4ce8a3a221b0b7fc17c10551e06
8th713 <8th713@gmail.com>
2018-01-30 22:54:49 +0900

    add application entry point

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { useStrict } from 'mobx'
+import { AddonService } from './service/AddonService'
+import { DOMPageService } from './service/DOMPageService'
+import { LocalStorageService } from './service/LocalStorageService'
+import { PixivClientService } from './service/PixivClientService'
+import { TwitterService } from './service/TwitterService'
+import { KeyboardShortcutService } from './service/KeyboardShortcutService'
+import { AppStore } from './store/AppStore'
+import { render } from './ui'
+
+useStrict(true)
+
+const services = {
+  page: new DOMPageService(),
+  storage: new LocalStorageService('cockpit'),
+  client: new PixivClientService(pixiv),
+  share: new TwitterService(),
+  shortcut: new KeyboardShortcutService(),
+  addon: new AddonService()
+}
+
+const appStore = new AppStore(services)
+
+services.page.injectGlobalCSS()
+services.page.onSelect(element => {
+  appStore.setElement(element)
+})
+
+render(appStore)
