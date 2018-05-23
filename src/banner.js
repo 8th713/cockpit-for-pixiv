47ebf42b83884b95d2291d5c20548bdccc360ea5
8th713 <8th713@gmail.com>
2018-05-23 23:10:06 +0900

    fix webpack config

--- src/banner.js
+++ src/banner.js
@@ -12,3 +12,24 @@
 // @exclude      https://www.pixiv.net/member_illust.php?mode*
 // @grant        none
 // ==/UserScript==
+
+// prettier-ignore
+const supportedSelectors = [
+  // old style
+  '._work',
+  // new style
+  '.bBzsEVG',
+  // /bookmark_new_illust.php
+  // /discovery
+  '.gtm-thumbnail-link',
+  // /discovery/users
+  '.JY31BOE',
+  // /showcase
+  '._3zE7tZ-',
+  // /howto
+  '._3sroO25',
+  // /contest
+  '._work-modal-target:not(.title)',
+  // /history.php
+  '._history-item.show-detail'
+]; // Do not remove the semicolon!!
