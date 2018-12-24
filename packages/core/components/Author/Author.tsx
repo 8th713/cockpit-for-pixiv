0bb43068f3745750d4036e2169861c8aa2bb7dab
8th713 <8th713@gmail.com>
2018-12-24 12:04:41 +0900

    add Author component

---
+++ packages/core/components/Author/Author.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+import { color } from '../theme'
+import { UserProvider } from '../../contexts'
+import { Row } from './Row'
+import { FollowButton } from './FollowButton'
+
+type Props = {
+  userId: string
+  children?: never
+}
+
+export const Author = React.memo(function Author({ userId }: Props) {
+  return (
+    <UserProvider userId={userId}>
+      <Layout>
+        <Row />
+        <FollowButton />
+      </Layout>
+    </UserProvider>
+  )
+})
+
+const Layout = styled.div`
+  all: unset;
+  position: sticky;
+  top: 0;
+  display: grid;
+  grid-template-rows: 56px 56px;
+  align-items: center;
+  background-color: ${color.surface};
+  color: ${color.surfaceText};
+`
---
+++ packages/core/components/Author/Row.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components'
+import { color, ripple } from '../theme'
+import { AsyncStatus } from '../../interfaces'
+import { UserProvider } from '../../contexts'
+import { Progress } from '../shared/Progress'
+import { AccountError } from '../shared/Icon'
+
+export const Row = React.memo(function Row() {
+  const result = UserProvider.useValue()
+
+  switch (result.status) {
+    case AsyncStatus.Loading: {
+      return (
+        <Layout>
+          <Progress size={40} />
+          読み込み中
+        </Layout>
+      )
+    }
+    case AsyncStatus.Success: {
+      const { value } = result
+
+      return (
+        <Layout href={`https://www.pixiv.net/member.php?id=${value.userId}`}>
+          <Avatar src={value.image} />
+          {value.name}
+        </Layout>
+      )
+    }
+    case AsyncStatus.Failure: {
+      return (
+        <Layout>
+          <AccountError width="40" height="40" />
+          取得できませんでした
+        </Layout>
+      )
+    }
+  }
+})
+
+const Layout = styled.a`
+  all: unset;
+  cursor: pointer;
+  height: 100%;
+  display: grid;
+  align-items: center;
+  grid-template-columns: 40px 1fr;
+  gap: 16px;
+  color: ${color.surfaceText};
+  ${ripple};
+`
+const Avatar = styled.img`
+  all: unset;
+  user-select: none;
+  object-fit: cover;
+  width: 40px;
+  height: 40px;
+  background-color: ${color.surfaceText};
+  border-radius: 50%;
+`
