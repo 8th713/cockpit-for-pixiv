256f6236316f5d0f061ca027e15ba3a1ee64a9e1
8th713 <8th713@gmail.com>
2018-12-27 19:12:06 +0900

    disable range requests

--- packages/core/externals/loadZip.ts
+++ packages/core/externals/loadZip.ts
@@ -1,4 +1,4 @@
-import wretch, { Wretcher } from 'wretch'
+import wretch from 'wretch'
 import { Ugoira, Frame } from '../interfaces'
 import { createPool } from './threadPool'
 
@@ -18,123 +18,78 @@ export async function loadZip(
   const client = wretch(ugoira.originalSrc, {
     credentials: 'same-origin'
   }).signal(ac)
-  const length = await getSize(client)
-  const eocd = await getEOCD(client, length)
-  const entries = await getEntries(client, eocd)
+
+  const response = await client.get().res()
+  const length = Number(response.headers.get('content-length'))
+  const buffer = await response.arrayBuffer()
+  const eocd = getEOCD(buffer, length)
+  const entries = getEntries(buffer, eocd)
 
   return Promise.all(
     entries.map(entry =>
-      pool.execute(() => getSrc(client, entry, ugoira.mime_type))
+      pool.execute(() => getSrc(buffer, entry, ugoira.mime_type))
     )
   ).then(images =>
     ugoira.frames.map((frame, i) => ({ ...frame, image: images[i] }))
   )
 }
+function getEOCD(buffer: ArrayBuffer, length: number): EOCD {
+  const view = new DataView(buffer, length - 22, 22)
 
-function fetchRange(client: Wretcher, begin: number, end: number) {
-  return client
-    .headers({ Range: `bytes=${begin}-${end - 1}` })
-    .get()
-    .arrayBuffer()
-}
-async function getSize(client: Wretcher) {
-  try {
-    const size = await client
-      .head()
-      .res(response => Number(response.headers.get('Content-Length') || 0))
-
-    return size
-  } catch {
-    throw new Error('zip ファイルのサイズ取得に失敗')
+  if (view.getUint32(0, true) !== 0x06054b50) {
+    throw new Error('End of Central Directory signature not found')
   }
+  const cdCount = view.getUint16(10, true) // セントラルディレクトリレコードの合計数
+  const cdSize = view.getUint32(12, true) // セントラルディレクトリのサイズ (バイト)
+  const cdOffset = view.getUint32(16, true) // セントラルディレクトリの開始位置のオフセット
+
+  return [cdCount, cdOffset, cdSize]
 }
-async function getEOCD(client: Wretcher, length: number): Promise<EOCD> {
-  const begin = length - 30000
+function getEntries(buffer: ArrayBuffer, [count, offset, size]: EOCD) {
+  const view = new DataView(buffer, offset, size)
+  const entries: Array<Entry> = []
+  let p = 0
 
-  try {
-    const eocd = await fetchRange(client, begin, length)
-    const view = new DataView(eocd, eocd.byteLength - 22, 22)
+  for (var i = 0; i < count; i++) {
+    if (view.getUint32(p, true) === 0x02014b50) {
+      const length = view.getUint32(p + 24, true) // 非圧縮サイズ
+      const begin = view.getUint32(p + 42, true) // ローカルファイルヘッダの相対オフセット
+      const extraLength = view.getUint16(p + 30, true) // 拡張フィールドの長さ
+      const cmtLength = view.getUint16(p + 32, true) // ファイルコメントの長さ
+      const nameLength = view.getUint16(p + 28, true) // ファイル名の長さ
+      const nameView = new Uint8Array(buffer, offset + p + 46, nameLength)
+      const name = String.fromCharCode(...nameView)
+      const size = length + nameLength + extraLength + 30
 
-    if (view.getUint32(0, true) !== 0x06054b50) {
-      throw new Error('End of Central Directory signature not found')
+      entries.push({ name, size, begin })
+      p += nameLength + extraLength + cmtLength + 46
+    } else {
+      console.error(`インデックス ${i} のエントリが取得できませんでした`)
+      throw new Error(`インデックス ${i} のエントリが取得できませんでした`)
     }
-    const cdCount = view.getUint16(10, true) // セントラルディレクトリレコードの合計数
-    const cdSize = view.getUint32(12, true) // セントラルディレクトリのサイズ (バイト)
-    const cdOffset = view.getUint32(16, true) // セントラルディレクトリの開始位置のオフセット
-
-    return [cdCount, cdSize, cdOffset]
-  } catch {
-    throw new Error('EOCD 取得に失敗')
   }
+  return entries
 }
-async function getEntries(
-  client: Wretcher,
-  [count, size, offset]: [number, number, number]
-) {
-  try {
-    const chunk = await fetchRange(client, offset, offset + size)
-    const view = new DataView(chunk)
-    const entries: Array<Entry> = []
-    let p = 0
-
-    for (var i = 0; i < count; i++) {
-      if (view.getUint32(p, true) === 0x02014b50) {
-        const length = view.getUint32(p + 24, true) // 非圧縮サイズ
-        const begin = view.getUint32(p + 42, true) // ローカルファイルヘッダの相対オフセット
-        const extraLength = view.getUint16(p + 30, true) // 拡張フィールドの長さ
-        const cmtLength = view.getUint16(p + 32, true) // ファイルコメントの長さ
-        const nameLength = view.getUint16(p + 28, true) // ファイル名の長さ
-        const nameView = new Uint8Array(chunk, p + 46, nameLength)
-        const name = String.fromCharCode(...nameView)
-        const size = length + nameLength + extraLength + 30
-
-        entries.push({ name, size, begin })
-        p += nameLength + extraLength + cmtLength + 46
-      } else {
-        console.error(`インデックス ${i} のエントリが取得できませんでした`)
-        throw new Error(`インデックス ${i} のエントリが取得できませんでした`)
-      }
-    }
-    return entries
-  } catch {
-    throw new Error('セントラルディレクトリエントリ取得に失敗')
-  }
-}
-async function getSrc(client: Wretcher, entry: Entry, type: string) {
-  try {
-    const chunk = await fetchRange(
-      client,
-      entry.begin,
-      entry.begin + entry.size
-    )
-    const view = new DataView(chunk)
-    const nameLength = view.getUint16(26, true)
-    const extraLength = view.getUint16(28, true)
-    const begin = nameLength + extraLength + 30
-    const blob = new Blob([chunk.slice(begin)], { type })
+function getSrc(buffer: ArrayBuffer, entry: Entry, type: string) {
+  return new Promise<HTMLImageElement>((resolve, reject) => {
+    const view = new DataView(buffer, entry.begin, entry.size)
+    const length = view.getUint32(22, true) // 非圧縮サイズ
+    const nameLength = view.getUint16(26, true) // ファイル名の長さ (n)
+    const extraLength = view.getUint16(28, true) // 拡張フィールドの長さ (m)
+    const begin = entry.begin + nameLength + extraLength + 30
+    const end = begin + length
+    const blob = new Blob([buffer.slice(begin, end)], { type })
     const src = URL.createObjectURL(blob)
-    const img = new Promise<HTMLImageElement>((resolve, reject) => {
-      const img = new Image()
+    const img = new Image()
 
-      img.addEventListener('load', () => {
-        URL.revokeObjectURL(src)
-        resolve(img)
-      })
-      img.addEventListener('error', () => {
-        URL.revokeObjectURL(src)
-        reject(img)
-      })
-      img.src = src
+    img.addEventListener('load', () => {
+      URL.revokeObjectURL(src)
+      resolve(img)
     })
-
-    return img
-  } catch (error) {
-    if (error instanceof Image) {
-      throw new Error('blob URL が不正です')
-    }
-    throw new Error(
-      `zip 内ファイルの取得に失敗 ${entry.name}: ${entry.begin}-${entry.begin +
-        entry.size}`
-    )
-  }
+    img.addEventListener('error', () => {
+      URL.revokeObjectURL(src)
+      reject(img)
+    })
+    img.src = src
+  })
 }
