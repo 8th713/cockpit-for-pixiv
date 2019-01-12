cf52440eb5b063c1a7d780432b62c358fb333a9f
8th713 <8th713@gmail.com>
2019-01-12 20:34:24 +0900

    fix format
    * This change makes editing easier even after minify

--- src/banner.js
+++ src/banner.js
@@ -16,43 +16,60 @@
 
 // prettier-ignore
 const supportedSelectors = [
-  // /member.php
-  // /member_illust.php
-  // /bookmark.php?id=xxx
-  // /member_illust.php?mode=medium
-  'a.sc-kEYyzF',
+/**!
+ * /member.php
+ * /member_illust.php
+ * /bookmark.php?id=xxx
+ * /member_illust.php?mode=medium
+ */
+'a.sc-kEYyzF',
 
-  // /search.php
-  // /bookmark_new_illust.php
-  // /discovery
-  // /idea // 今日のお題新着
-  'a.PKslhVT',
+/**!
+ * /search.php
+ * /bookmark_new_illust.php
+ * /discovery
+ * /idea  (今日のお題新着)
+ */
+'a._1wlaFo6',
 
-  // /discovery/users
-  'a._3dXyksb',
+/**!
+ * /discovery/users
+ */
+'a.JY31BOE',
 
-  // /howto
-  'a.o8jYSOX',
+/**!
+ * /howto
+ */
+'a._3sroO25',
 
-  // /contest
-  'a._work-modal-target:not(.title)',
+/**!
+ * /contest
+ */
+'a._work-modal-target:not(.title)',
 
-  // /showcase(pixivisionに移転した?)
-  'a._3zE7tZ-',
+/**!
+ * /history.php
+ */
+'a._history-item.show-detail',
+'a._history-related-item',
 
-  // /history.php
-  'a._history-item.show-detail',
-  'a._history-related-item',
+/**!
+ * /  (フォロー新着作品,おすすめ作品)
+ * /ranking.php
+ * /bookmark.php
+ * /new_illust.php
+ * /tags.php  (全期間の人気イラスト,新着イラスト)
+ * /stacc
+ * /idea  (ユーザー企画,新着の企画目録)
+ */
+'a._work',
 
-  // / // フォロー新着作品,おすすめ作品
-  // /ranking.php
-  // /bookmark.php
-  // /new_illust.php
-  // /tags.php // 全期間の人気イラスト,新着イラスト
-  // /stacc
-  // /idea // ユーザー企画,新着の企画目録
-  'a._work',
+/**!
+ * /tags.php  (週間の人気イラスト)
+ */
+'a.work'
 
-  // /tags.php // 週間の人気イラスト
-  'a.work',
-]; // Do not remove the semicolon!!
+/**!
+ * Do not remove the semicolon!!
+ */
+];
