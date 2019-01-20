5112ea0a6c3738c3167a54e645baf21f40148875
8th713 <8th713@gmail.com>
2019-01-20 12:04:11 +0900

    prerelease: v4.0.0-alpha.6

--- docs/cockpit-download-addon.user.js
+++ docs/cockpit-download-addon.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         cockpit download addon
 // @description  cockpit download helper addon.
-// @version      4.0.0-alpha.5
+// @version      4.0.0-alpha.6
 // @author       8th713
 // @homepage     https://github.com/8th713/cockpit-for-pixiv
 // @supportURL   https://github.com/8th713/cockpit-for-pixiv/issues
