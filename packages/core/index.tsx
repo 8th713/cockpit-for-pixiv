e7494000c116cd10c75b1a7e39a4dd87a63e7c3b
8th713 <8th713@gmail.com>
2019-02-04 19:20:37 +0900

    enable concurrent mode

--- packages/core/index.tsx
+++ packages/core/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './components/App'
 
-ReactDOM.render(
-  <App />,
+// ReactDOM.render(
+//   <App />,
+//   document.body.appendChild(document.createElement('div'))
+// )
+
+ReactDOM.createRoot(
   document.body.appendChild(document.createElement('div'))
-)
+).render(<App />)
