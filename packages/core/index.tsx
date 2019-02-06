9c37b6904423162722f1a86030539afc2846021a
8th713 <8th713@gmail.com>
2019-02-06 18:49:33 +0900

    disabled concurrent mode

--- packages/core/index.tsx
+++ packages/core/index.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './components/App'
 
-// ReactDOM.render(
-//   <App />,
-//   document.body.appendChild(document.createElement('div'))
-// )
-
-ReactDOM.createRoot(
+ReactDOM.render(
+  <App />,
   document.body.appendChild(document.createElement('div'))
-).render(<App />)
+)
