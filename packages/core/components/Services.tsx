5e8d6d595e1113cb40b94bf5f877d68ccb83d349
8th713 <8th713@gmail.com>
2019-07-02 20:53:25 +0900

    add Services

---
+++ packages/core/components/Services.tsx
@@ -0,0 +1,30 @@
+import React, { useContext, useDebugValue } from 'react'
+import { AddonStore } from '../externals/addonStore'
+import { APIClient } from '../externals/apiClient'
+
+export type ServicesProps = {
+  addonStore: AddonStore
+  apiClient: APIClient
+}
+
+type Props = ServicesProps & {
+  children?: React.ReactNode
+}
+
+const NO_PROVIDER = 'Missing Services'
+const ValueContext = React.createContext<ServicesProps | typeof NO_PROVIDER>(
+  NO_PROVIDER
+)
+
+export function useServices() {
+  const value = useContext(ValueContext)
+  if (value === NO_PROVIDER) {
+    throw new Error(NO_PROVIDER)
+  }
+  useDebugValue(value)
+  return value
+}
+
+export function Services({ children, ...props }: Props) {
+  return <ValueContext.Provider value={props}>{children}</ValueContext.Provider>
+}
