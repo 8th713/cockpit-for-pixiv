d96cc533ba0f617f106ffb11f81b1a33a4aecb76
8th713 <8th713@gmail.com>
2019-07-02 20:47:03 +0900

    update project

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,6 +46,7 @@ module.exports = {
     'jsx-a11y/role-has-required-aria-props': 'warn',
     'jsx-a11y/role-supports-aria-props': 'warn',
     'jsx-a11y/scope': 'warn',
-    'react-hooks/rules-of-hooks': 'warn'
+    'react-hooks/rules-of-hooks': 'warn',
+    'react-hooks/exhaustive-deps': 'warn'
   }
 }
