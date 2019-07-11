6c6936f12490c669c8417eafd7b591b11f41e91f
8th713 <8th713@gmail.com>
2019-07-11 20:37:07 +0900

    improve Switch component by styled-system

--- packages/core/components/shared/Switch.tsx
+++ packages/core/components/shared/Switch.tsx
@@ -1,40 +1,56 @@
 import React from 'react'
 import styled from 'styled-components'
+import * as sys from '../system'
 
-type Props = Omit<React.ComponentPropsWithoutRef<'input'>, 'type'>
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
-export const Switch = React.forwardRef<HTMLInputElement, Props>(function Switch(
-  props,
-  ref
-) {
-  const { className, ...rest } = props
-  const { disabled } = rest
+interface SystemProps
+  extends sys.MarginProps,
+    sys.PositionProps,
+    sys.FlexItemProps,
+    sys.GridItemProps {}
+type NativeProps = Omit<React.ComponentPropsWithoutRef<'input'>, 'type'>
+export type SwitchProps = SystemProps & NativeProps & { as?: never }
+
+const Impl = React.forwardRef<HTMLInputElement, SwitchProps>((props, ref) => {
+  const otherProps = sys.omitSystemProps(props)
+  const { className, ...inputProps } = otherProps
+  const { disabled } = inputProps
 
   return (
-    <Root aria-disabled={disabled} className={className}>
-      <Input ref={ref} {...rest} type="checkbox" />
+    <div className={className} aria-disabled={disabled}>
+      <Input ref={ref} {...inputProps} type="checkbox" />
       <Track />
       <Thumb />
-    </Root>
+    </div>
   )
 })
+Impl.displayName = 'Switch'
 
-const Root = styled.span`
+export const Switch = styled(Impl)`
   box-sizing: border-box;
   position: relative;
   display: inline-flex;
   width: 58px;
   height: 38px;
-  flex-shrink: 0;
   padding: 12px;
+  flex: 0 0 auto;
   &[aria-disabled='true'] {
     pointer-events: none;
     opacity: var(--disabled);
   }
+  ${sys.compose(
+    sys.margin,
+    sys.position,
+    sys.flexItem,
+    sys.gridItem
+  )}
 `
+
 const Input = styled.input`
   appearance: none;
   cursor: pointer;
+  box-sizing: border-box;
   position: absolute;
   top: 0;
   left: 0;
@@ -48,9 +64,10 @@ const Input = styled.input`
   }
 `
 const Track = styled.span`
+  box-sizing: border-box;
   width: 100%;
   height: 100%;
-  opacity: 0.54;
+  opacity: 0.3;
   transition: background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
   border-radius: 7px;
   background-color: #fff;
@@ -59,14 +76,17 @@ const Track = styled.span`
   }
 `
 const Thumb = styled.span`
+  box-sizing: border-box;
   position: absolute;
   top: 0;
   left: 0;
   padding: 9px;
   color: var(--on-surface);
-  transition: transform 150ms cubic-bezier(0.4, 0, 0.2, 1);
+  transition: color 150ms cubic-bezier(0.4, 0, 0.2, 1),
+    transform 150ms cubic-bezier(0.4, 0, 0.2, 1);
   ::before {
     content: '';
+    box-sizing: border-box;
     display: flex;
     width: 20px;
     height: 20px;
@@ -75,6 +95,7 @@ const Thumb = styled.span`
   }
   ::after {
     content: '';
+    box-sizing: border-box;
     position: absolute;
     top: 0;
     left: 0;
@@ -83,6 +104,7 @@ const Thumb = styled.span`
     border-radius: 50%;
     background-color: currentColor;
     opacity: 0;
+    transition: opacity 15ms linear;
   }
   input:hover ~ & {
     &:after {
