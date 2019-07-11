8afd1e13d284423db739119b7ca7be92f61ddf38
8th713 <8th713@gmail.com>
2019-07-11 20:36:48 +0900

    improve Progress component by styled-system

--- packages/core/components/shared/Progress.tsx
+++ packages/core/components/shared/Progress.tsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
+import * as sys from '../system'
 
-type Props = React.ComponentPropsWithoutRef<'div'> & {
-  size?: number
-  children?: never
-}
+interface BoxProps
+  extends sys.PositionProps,
+    sys.SizeProps,
+    sys.MarginProps,
+    sys.FlexItemProps,
+    sys.GridItemProps,
+    sys.ColorProps {}
+type DivProps = React.ComponentPropsWithoutRef<'div'>
+export type ProgressProps = BoxProps &
+  DivProps & {
+    as?: never
+  }
 
-export function Progress({ size = 256, ...props }: Props) {
-  const style: React.CSSProperties = {
-    '--size': `${size}px`,
-    ...props.style
-  } as any
-  return (
-    <Root {...props} role="progressbar" style={style}>
-      <svg viewBox="0 0 50 50">
-        <circle fill="none" cx={25} cy={25} r={20} strokeWidth={3.6} />
-      </svg>
-    </Root>
-  )
-}
+const Impl = React.forwardRef<HTMLDivElement, ProgressProps>((props, ref) => (
+  <div {...props} role="progressbar">
+    <svg viewBox="0 0 50 50">
+      <circle fill="none" cx={25} cy={25} r={20} strokeWidth={3.6} />
+    </svg>
+  </div>
+))
+Impl.displayName = 'Progress'
 
 const rotate = keyframes`
   100% {
@@ -41,15 +45,17 @@ const dash = keyframes`
   }
 `
 
-const Root = styled.div`
-  display: block;
-  justify-self: center;
-  align-self: center;
-  width: var(--size);
-  height: var(--size);
-  margin: auto;
-  color: var(--primary);
-
+export const Progress = styled(Impl)`
+  box-sizing: border-box;
+  flex-shrink: 0;
+  ${sys.compose(
+    sys.position,
+    sys.size,
+    sys.margin,
+    sys.flexItem,
+    sys.gridItem,
+    sys.color
+  )}
   & > svg {
     animation: ${rotate} 1.4s linear infinite;
     & > circle {
@@ -61,3 +67,8 @@ const Root = styled.div`
     }
   }
 `
+Progress.defaultProps = {
+  size: 256,
+  m: 'auto',
+  color: 'primary'
+}
