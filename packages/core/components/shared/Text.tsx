b5e2b64c3ccd829ea229c0cf13371a5159954040
8th713 <8th713@gmail.com>
2019-07-11 20:34:30 +0900

    improve Text component by styled-system

--- packages/core/components/shared/Text.tsx
+++ packages/core/components/shared/Text.tsx
@@ -1,105 +1,41 @@
-import React from 'react'
 import styled from 'styled-components'
-import { fontPresets } from './styles'
+import * as sys from '../system'
+import { Box, BoxProps } from './Box'
 
-type Color = 'primary' | 'secondary' | 'error'
-type Kind = 'h1' | 'h2' | 'h3' | 'b1' | 'b2' | 'button' | 'caption' | 'overline'
+export interface TextProps
+  extends BoxProps,
+    sys.STextStyleProps,
+    sys.EmProps,
+    sys.EllipsisProps {}
 
-type Props = React.ComponentPropsWithoutRef<'span'> & {
-  as?: keyof JSX.IntrinsicElements
-  color?: Color
-  kind?: Kind
-  low?: boolean
-  noWrap?: boolean
-  margin?: boolean
+const getAs = ({ textStyle }: TextProps) => {
+  switch (textStyle) {
+    case 'h1':
+    case 'h2':
+    case 'h3':
+      return { as: textStyle }
+    case 'body1':
+    case 'body2':
+    case 'b1':
+    case 'b2':
+      return { as: 'p' }
+    default:
+      return { as: 'span' }
+  }
 }
 
-export const Text = React.forwardRef<HTMLParagraphElement, Props>(
-  ({ as, color, kind = 'b1', low, noWrap, margin, ...props }, ref) => {
-    switch (kind) {
-      case 'h1':
-      case 'h2':
-      case 'h3':
-        as = as || kind
-        break
-      case 'b1':
-      case 'b2':
-        as = as || 'p'
-        break
-      default:
-        break
-    }
-    return (
-      <Root
-        ref={ref}
-        as={as}
-        data-no-wrap={noWrap}
-        data-low={low}
-        data-color={color}
-        data-kind={kind}
-        {...props}
-        style={{ marginBottom: margin ? 16 : undefined, ...props.style }}
-      />
-    )
-  }
-)
-
-const Root = styled.span`
-  margin: 0;
-  padding: 0;
-  border: 0;
-  color: var(--on-surface);
+export const Text = styled(Box).attrs(getAs)<TextProps>`
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
-  font-family: 'Roboto', 'Helvetica Neue', 'arial', 'Noto Sans CJK JP',
-    'Hiragino Kaku Gothic ProN', Meiryo, sans-serif;
-  ${fontPresets.body1};
-
-  &:not([data-color]) {
-    opacity: var(--high);
-  }
-  &[data-no-wrap='true'] {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-  &[data-low='true']:not([data-color]) {
-    opacity: var(--medium);
-  }
-  &[data-color='primary'] {
-    color: var(--primary);
-  }
-  &[data-color='secondary'] {
-    color: var(--secondary);
-  }
-  &[data-color='error'] {
-    color: var(--error);
-  }
-  &[data-kind='h1'] {
-    opacity: 1;
-    ${fontPresets.h1};
-  }
-  &[data-kind='h2'] {
-    opacity: 1;
-    ${fontPresets.h2};
-  }
-  &[data-kind='h3'] {
-    opacity: 1;
-    ${fontPresets.h3};
-  }
-  &[data-kind='b1'] {
-    ${fontPresets.body1};
-  }
-  &[data-kind='b2'] {
-    ${fontPresets.body2};
-  }
-  &[data-kind='button'] {
-    ${fontPresets.button};
-  }
-  &[data-kind='caption'] {
-    ${fontPresets.caption};
-  }
-  &[data-kind='overline'] {
-    ${fontPresets.overline};
-  }
+  font-family: Roboto, Helvetica Neue, arial, Noto Sans CJK JP,
+    Hiragino Kaku Gothic ProN, Meiryo, sans-serif;
+  ${sys.compose(
+    sys.textStyle,
+    sys.em
+  )};
+  ${sys.ellipsis};
 `
+Text.displayName = 'Text'
+Text.defaultProps = {
+  textStyle: 'body1'
+}
