a41bce1f38e4f9646fe9f2617285c9e87c4d7064
8th713 <8th713@gmail.com>
2019-07-11 20:38:16 +0900

    add ThemeProvider

--- packages/core/components/App.tsx
+++ packages/core/components/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
 import { About, AboutHost } from './About'
 import { GlobalStyle } from './GlobalStyle'
 import { Info } from './Info'
 import { Router } from './Router'
 import { Services, ServicesProps } from './Services'
+import { theme } from './theme'
 import { Viewer } from './Viewer'
 
 type Props = {
@@ -12,18 +14,20 @@ type Props = {
 
 export function App({ services }: Props) {
   return (
-    <Services {...services}>
-      <GlobalStyle />
-      <AboutHost>
-        <Router>
-          {id => (
-            <Viewer id={id}>
-              <Info />
-            </Viewer>
-          )}
-        </Router>
-        <About />
-      </AboutHost>
-    </Services>
+    <ThemeProvider theme={theme}>
+      <Services {...services}>
+        <GlobalStyle />
+        <AboutHost>
+          <Router>
+            {id => (
+              <Viewer id={id}>
+                <Info />
+              </Viewer>
+            )}
+          </Router>
+          <About />
+        </AboutHost>
+      </Services>
+    </ThemeProvider>
   )
 }
