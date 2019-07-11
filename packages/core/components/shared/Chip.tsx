bb6a50d5e4828755a6df67e422494f019ac09770
8th713 <8th713@gmail.com>
2019-07-11 20:35:58 +0900

    improve Chip component by styled-system

--- packages/core/components/shared/Chip.tsx
+++ packages/core/components/shared/Chip.tsx
@@ -1,43 +1,17 @@
-import React from 'react'
 import styled from 'styled-components'
+import { Text } from './Text'
 
-type Props = {
-  className?: string
-  children?: React.ReactNode
-}
-
-export function Chip({ className, children }: Props) {
-  return (
-    <Root className={className}>
-      <Text>{children}</Text>
-    </Root>
-  )
-}
-
-const Root = styled.div`
-  cursor: default;
-  box-sizing: border-box;
-  outline: none;
+export const Chip = styled(Text)`
+  user-select: none;
   display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  height: 24px;
-  padding: 0;
-  border: 0;
+  min-width: 88px;
+  padding: 4px 16px;
   border-radius: 16px;
   background-color: #616161;
   color: #fff;
-  font-size: 12px;
-  vertical-align: middle;
-  text-decoration: none;
   white-space: nowrap;
+  justify-content: center;
 `
-const Text = styled.span`
-  cursor: inherit;
-  user-select: none;
-  display: flex;
-  align-items: center;
-  padding-left: 8px;
-  padding-right: 8px;
-  white-space: nowrap;
-`
+Chip.defaultProps = {
+  textStyle: 'caption'
+}
