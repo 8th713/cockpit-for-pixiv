5d957b647888e80474085a4a63a32fe26696bf3a
8th713 <8th713@gmail.com>
2019-07-27 09:15:09 +0900

    replace jump link to direct link

--- packages/core/components/Info/Description/Comment.tsx
+++ packages/core/components/Info/Description/Comment.tsx
@@ -4,6 +4,19 @@ import { useRouteId } from '../../Router'
 import { useServices } from '../../Services'
 import { Text } from '../../shared'
 
+const ANKER_TAG = /\<a href=".+?"/
+const replaceJumpLink = (comment: string) => {
+  const doc = new DOMParser().parseFromString(comment, 'text/html')
+  const links = doc.getElementsByTagName('a')
+  for (const link of links) {
+    if (link.pathname === '/jump.php') {
+      link.href = unescape(link.search.slice(1))
+      link.referrerPolicy = 'no-referrer'
+    }
+  }
+  return doc.body.innerHTML
+}
+
 export function Comment() {
   const { useIllust } = useServices()
   const id = useRouteId()
@@ -12,7 +25,10 @@ export function Comment() {
   if (!illust) return null
   if (!illust.illustComment) return null
 
-  const comment = illust.illustComment.trim()
+  let comment = illust.illustComment
+  if (ANKER_TAG.test(comment)) {
+    comment = replaceJumpLink(illust.illustComment)
+  }
 
   return <Root textStyle="b2" dangerouslySetInnerHTML={{ __html: comment }} />
 }
