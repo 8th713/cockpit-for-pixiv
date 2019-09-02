6cb1ac1c0175d01b3aeaea014458a6f834e23765
8th713 <8th713@gmail.com>
2019-09-02 18:02:06 +0900

    change string literal to object style
    * createGlobalStyle does not apply minify

--- packages/core/components/GlobalStyle.tsx
+++ packages/core/components/GlobalStyle.tsx
@@ -1,12 +1,11 @@
 import { createGlobalStyle } from 'styled-components'
 import { NO_SCROLLBAR } from '../constants'
 
-export const GlobalStyle = createGlobalStyle`
-  html.${NO_SCROLLBAR} {
-    overflow: hidden;
-    & iframe,
-    & embed {
-      visibility: hidden;
+export const GlobalStyle = createGlobalStyle({
+  [`html.${NO_SCROLLBAR}`]: {
+    overflow: 'hidden',
+    '& iframe, & embed': {
+      visibility: 'hidden'
     }
   }
-`
+})
