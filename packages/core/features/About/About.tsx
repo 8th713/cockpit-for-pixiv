08ab9da821129a935a348f9de8b17610b07f012a
8th713 <8th713@gmail.com>
2019-09-16 03:27:04 +0900

    remove AboutValue context

--- packages/core/features/About/About.tsx
+++ packages/core/features/About/About.tsx
@@ -1,21 +1,14 @@
-import React, { useContext, useMemo, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Hotkey } from '../../components'
 import { KEY_ASSIGNMENT } from '../../constants'
+import { AboutDialog } from './AboutDialog'
 
 type Props = { children?: React.ReactNode }
-type Value = readonly [boolean, SetState]
 type SetState = React.Dispatch<React.SetStateAction<boolean>>
 
-const AboutValue = React.createContext<Value | null>(null)
 const AboutUpdate = React.createContext<SetState | null>(null)
-AboutValue.displayName = 'AboutValue'
 AboutUpdate.displayName = 'AboutUpdate'
 
-export const useAbout = () => {
-  const value = useContext(AboutValue)
-  if (value === null) throw new Error('Missing AboutProvider')
-  return value
-}
 export const useUpdateAbout = () => {
   const value = useContext(AboutUpdate)
   if (value === null) throw new Error('Missing AboutProvider')
@@ -23,15 +16,14 @@ export const useUpdateAbout = () => {
 }
 export const About = ({ children }: Props) => {
   const [open, setOpen] = useState(false)
-  const value = useMemo(() => [open, setOpen] as const, [open])
   const toggle = () => setOpen(v => !v)
+  const close = () => setOpen(false)
 
   return (
     <AboutUpdate.Provider value={setOpen}>
-      <AboutValue.Provider value={value}>
-        {children}
-        <Hotkey {...KEY_ASSIGNMENT.help} action={toggle} />
-      </AboutValue.Provider>
+      {children}
+      <Hotkey {...KEY_ASSIGNMENT.help} action={toggle} />
+      <AboutDialog open={open} onClose={close} />
     </AboutUpdate.Provider>
   )
 }
