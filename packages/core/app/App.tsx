89edc698af601ef43e95969219b1cb0b1563f2ed
8th713 <8th713@gmail.com>
2019-09-19 19:21:26 +0900

    use sStyleSheetManager
    * add extraScopePlugin
    * disable vender prefixes

--- packages/core/app/App.tsx
+++ packages/core/app/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
+import { StyleSheetManager, ThemeProvider } from 'styled-components'
 import { Box, Divider } from '../components'
 import { AddonStore } from '../externals/addonStore'
 import { About } from '../features/About'
@@ -16,52 +16,65 @@ import { User } from '../features/User'
 import { GlobalStyle } from './GlobalStyle'
 import { theme } from './theme'
 
-type Props = { addonStore: AddonStore }
+type Props = { stylisPlugins?: Function[]; addonStore: AddonStore }
 
-export const App = ({ addonStore }: Props) => (
-  <ThemeProvider theme={theme}>
-    <AddonProvider value={addonStore}>
-      <GlobalStyle />
-      <About>
-        <Router>
-          {illustId => (
-            <FullSizeMode>
-              <ScrollSpy illustId={illustId}>
-                <IOProvider>
-                  <StandardView illustId={illustId}>
-                    <Caption />
-                    <Box display="flex" flexDirection="column" bg="surface">
-                      <Divider mx={3} />
-                    </Box>
-                    <Box
-                      display="flex"
-                      p={3}
-                      alignItems="flex-start"
-                      bg="surface"
-                    >
-                      <Box flex="960px" maxWidth={960}>
-                        <Description />
-                      </Box>
-                      <Box pr={3} flexGrow={1} />
-                      <Box
-                        position="sticky"
-                        top="var(--caption-height)"
-                        flex="0 0 280px"
-                      >
-                        <User />
-                      </Box>
-                    </Box>
-                    <Box bg="surface">
-                      <RelatedWorks />
-                    </Box>
-                  </StandardView>
-                </IOProvider>
-                <FullSizeView illustId={illustId} />
-              </ScrollSpy>
-            </FullSizeMode>
-          )}
-        </Router>
-      </About>
-    </AddonProvider>
-  </ThemeProvider>
+const SheetManager = (StyleSheetManager as unknown) as React.FC<{
+  children?: React.ReactChild
+  disableCSSOMInjection?: boolean
+  disableVendorPrefixes?: boolean
+  sheet?: any
+  stylisPlugins?: Function[]
+  target?: HTMLElement
+}>
+
+export const App = ({ addonStore, stylisPlugins }: Props) => (
+  <>
+    <GlobalStyle />
+    <SheetManager disableVendorPrefixes stylisPlugins={stylisPlugins}>
+      <ThemeProvider theme={theme}>
+        <AddonProvider value={addonStore}>
+          <About>
+            <Router>
+              {illustId => (
+                <FullSizeMode>
+                  <ScrollSpy illustId={illustId}>
+                    <IOProvider>
+                      <StandardView illustId={illustId}>
+                        <Caption />
+                        <Box display="flex" flexDirection="column" bg="surface">
+                          <Divider mx={3} />
+                        </Box>
+                        <Box
+                          display="flex"
+                          p={3}
+                          alignItems="flex-start"
+                          bg="surface"
+                        >
+                          <Box flex="960px" maxWidth={960}>
+                            <Description />
+                          </Box>
+                          <Box pr={3} flexGrow={1} />
+                          <Box
+                            position="sticky"
+                            top="var(--caption-height)"
+                            flex="0 0 280px"
+                          >
+                            <User />
+                          </Box>
+                        </Box>
+                        <Box bg="surface">
+                          <RelatedWorks />
+                        </Box>
+                      </StandardView>
+                    </IOProvider>
+                    <FullSizeView illustId={illustId} />
+                  </ScrollSpy>
+                </FullSizeMode>
+              )}
+            </Router>
+          </About>
+        </AddonProvider>
+      </ThemeProvider>
+    </SheetManager>
+  </>
 )
--- packages/core/index.tsx
+++ packages/core/index.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './app/App'
 import { createAddonStore } from './externals/addonStore'
+import extraScopePlugin from 'stylis-plugin-extra-scope'
 
+const scope = '#cockpit-for-pixiv'
+const stylisPlugins = [extraScopePlugin(scope)]
 const addonStore = createAddonStore()
+const root = document.createElement('div')
+root.id = scope.slice(1)
 
 ReactDOM.render(
-  <App addonStore={addonStore} />,
-  document.body.appendChild(document.createElement('div'))
+  <App addonStore={addonStore} stylisPlugins={stylisPlugins} />,
+  document.body.appendChild(root)
 )
