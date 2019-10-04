f0aa7f09ba07f8244b4d44b11abf95df453d9c0e
8th713 <8th713@gmail.com>
2019-10-04 20:28:55 +0900

    redevelopment the theme

--- packages/core/app/theme.ts
+++ packages/core/app/theme.ts
@@ -1,153 +1,114 @@
-import { DefaultTheme, CSSObject } from 'styled-components'
-
-const body1: CSSObject = {
-  fontSize: '16px',
-  fontWeight: 400,
-  lineHeight: '1.5',
-  letterSpacing: '0.00938em'
-}
-const body2: CSSObject = {
-  fontSize: '14px',
-  fontWeight: 400,
-  lineHeight: '1.43',
-  letterSpacing: '0.01071em'
-}
+import { DefaultTheme } from 'styled-components'
 
 export const theme: DefaultTheme = {
+  breakpoints: [35, 60].map(v => v + 'em'),
   colors: {
-    surface: 'var(--surface)',
-    onSurface: 'var(--on-surface)',
-    primary: 'var(--primary)',
-    onPrimary: 'var(--on-primary)',
-    secondary: 'var(--secondary)',
-    onSecondary: 'var(--on-secondary)',
-    error: 'var(--error)',
-    onError: 'var(--on-error)'
+    bg: '#333',
+    surface: '#0b132b',
+    onSurface: '#fff',
+    primary: '#55cfff',
+    onPrimary: '#000',
+    secondary: '#f28d85',
+    onSecondary: '#000'
   },
-  colorStyles: {
-    primary: {
-      '--bg-color': 'var(--primary)',
-      '--text-color': 'var(--on-primary)',
-      '--text-opacity': 1
-    },
-    secondary: {
-      '--bg-color': 'var(--secondary)',
-      '--text-color': 'var(--on-secondary)',
-      '--text-opacity': 1
-    },
-    error: {
-      '--bg-color': 'var(--error)',
-      '--text-color': 'var(--on-error)',
-      '--text-opacity': 1
-    }
+  fonts: {
+    sans:
+      '"Roboto", "Helvetica Neue", "arial", "Noto Sans CJK JP", "Hiragino Kaku Gothic ProN", Meiryo, sans-serif',
+    mono:
+      "'Source Code Pro', SFMono-Regular, Consolas, 'Liberation Mono', Menlo, Courier, monospace"
   },
-  textStyles: {
+  fontSizes: {
+    h1: '20px',
+    h2: '16px',
+    h3: '14px',
+    body1: '16px',
+    body2: '14px',
+    caption: '12px',
+    button: '14px'
+  },
+  shadows: [
+    'none',
+    '0px 1px 3px 0px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 2px 1px -1px rgba(0,0,0,0.12)',
+    '0px 1px 5px 0px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 3px 1px -2px rgba(0,0,0,0.12)',
+    '0px 1px 8px 0px rgba(0,0,0,0.2),0px 3px 4px 0px rgba(0,0,0,0.14),0px 3px 3px -2px rgba(0,0,0,0.12)',
+    '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)',
+    '0px 3px 5px -1px rgba(0,0,0,0.2),0px 5px 8px 0px rgba(0,0,0,0.14),0px 1px 14px 0px rgba(0,0,0,0.12)',
+    '0px 3px 5px -1px rgba(0,0,0,0.2),0px 6px 10px 0px rgba(0,0,0,0.14),0px 1px 18px 0px rgba(0,0,0,0.12)',
+    '0px 4px 5px -2px rgba(0,0,0,0.2),0px 7px 10px 1px rgba(0,0,0,0.14),0px 2px 16px 1px rgba(0,0,0,0.12)',
+    '0px 5px 5px -3px rgba(0,0,0,0.2),0px 8px 10px 1px rgba(0,0,0,0.14),0px 3px 14px 2px rgba(0,0,0,0.12)',
+    '0px 5px 6px -3px rgba(0,0,0,0.2),0px 9px 12px 1px rgba(0,0,0,0.14),0px 3px 16px 2px rgba(0,0,0,0.12)',
+    '0px 6px 6px -3px rgba(0,0,0,0.2),0px 10px 14px 1px rgba(0,0,0,0.14),0px 4px 18px 3px rgba(0,0,0,0.12)',
+    '0px 6px 7px -4px rgba(0,0,0,0.2),0px 11px 15px 1px rgba(0,0,0,0.14),0px 4px 20px 3px rgba(0,0,0,0.12)',
+    '0px 7px 8px -4px rgba(0,0,0,0.2),0px 12px 17px 2px rgba(0,0,0,0.14),0px 5px 22px 4px rgba(0,0,0,0.12)',
+    '0px 7px 8px -4px rgba(0,0,0,0.2),0px 13px 19px 2px rgba(0,0,0,0.14),0px 5px 24px 4px rgba(0,0,0,0.12)',
+    '0px 7px 9px -4px rgba(0,0,0,0.2),0px 14px 21px 2px rgba(0,0,0,0.14),0px 5px 26px 4px rgba(0,0,0,0.12)',
+    '0px 8px 9px -5px rgba(0,0,0,0.2),0px 15px 22px 2px rgba(0,0,0,0.14),0px 6px 28px 5px rgba(0,0,0,0.12)',
+    '0px 8px 10px -5px rgba(0,0,0,0.2),0px 16px 24px 2px rgba(0,0,0,0.14),0px 6px 30px 5px rgba(0,0,0,0.12)',
+    '0px 8px 11px -5px rgba(0,0,0,0.2),0px 17px 26px 2px rgba(0,0,0,0.14),0px 6px 32px 5px rgba(0,0,0,0.12)',
+    '0px 9px 11px -5px rgba(0,0,0,0.2),0px 18px 28px 2px rgba(0,0,0,0.14),0px 7px 34px 6px rgba(0,0,0,0.12)',
+    '0px 9px 12px -6px rgba(0,0,0,0.2),0px 19px 29px 2px rgba(0,0,0,0.14),0px 7px 36px 6px rgba(0,0,0,0.12)',
+    '0px 10px 13px -6px rgba(0,0,0,0.2),0px 20px 31px 3px rgba(0,0,0,0.14),0px 8px 38px 7px rgba(0,0,0,0.12)',
+    '0px 10px 13px -6px rgba(0,0,0,0.2),0px 21px 33px 3px rgba(0,0,0,0.14),0px 8px 40px 7px rgba(0,0,0,0.12)',
+    '0px 10px 14px -6px rgba(0,0,0,0.2),0px 22px 35px 3px rgba(0,0,0,0.14),0px 8px 42px 7px rgba(0,0,0,0.12)',
+    '0px 11px 14px -7px rgba(0,0,0,0.2),0px 23px 36px 3px rgba(0,0,0,0.14),0px 9px 44px 8px rgba(0,0,0,0.12)',
+    '0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12)'
+  ],
+  opacities: {
+    hover: 0.12,
+    focus: 0.24,
+    divider: 0.12,
+    inactive: 0.6,
+    disabled: 0.38
+  },
+  text: {
     h1: {
-      fontSize: '20px',
+      fontFamily: 'sans',
+      fontSize: 'h1',
       fontWeight: 500,
-      lineHeight: '1.6',
+      lineHeight: 1.6,
       letterSpacing: '0.0075em'
     },
     h2: {
-      fontSize: '16px',
+      fontFamily: 'sans',
+      fontSize: 'h2',
       fontWeight: 400,
-      lineHeight: '1.75',
+      lineHeight: 1.75,
       letterSpacing: '0.00938em'
     },
     h3: {
-      fontSize: '14px',
+      fontFamily: 'sans',
+      fontSize: 'h3',
       fontWeight: 500,
-      lineHeight: '1.57',
+      lineHeight: 1.715,
       letterSpacing: '0.00714em'
     },
-    button: {
-      fontSize: '14px',
-      fontWeight: 500,
-      lineHeight: '1.75',
-      letterSpacing: '0.02857em',
-      textTransform: 'uppercase'
+    body1: {
+      fontFamily: 'sans',
+      fontSize: 'body1',
+      fontWeight: 400,
+      lineHeight: 1.5,
+      letterSpacing: '0.00938em'
+    },
+    body2: {
+      fontFamily: 'sans',
+      fontSize: 'body2',
+      fontWeight: 400,
+      lineHeight: 1.43,
+      letterSpacing: '0.01071em'
     },
     caption: {
-      fontSize: '12px',
+      fontFamily: 'sans',
+      fontSize: 'caption',
       fontWeight: 400,
-      lineHeight: '1.66',
+      lineHeight: 1.67,
       letterSpacing: '0.03333em'
     },
-    overline: {
-      fontSize: '12px',
-      fontWeight: 400,
-      lineHeight: '2.66',
-      letterSpacing: '0.08333em',
-      textTransform: 'uppercase'
-    },
-    body1,
-    body2,
-    b1: body1,
-    b2: body2
-  },
-  buttons: {
-    text: {
-      padding: '6px 8px',
-      color: 'var(--bg-color)',
-      ':disabled': {
-        color: 'var(--on-surface)'
-      }
-    },
-    outlined: {
-      color: 'var(--bg-color)',
-      // outline
-      '::after': {
-        content: '""',
-        boxSizing: 'border-box',
-        pointerEvents: 'none',
-        position: 'absolute',
-        top: '0',
-        left: '0',
-        width: '100%',
-        height: '100%',
-        border: '2px solid currentColor',
-        borderRadius: 'inherit',
-        transition: 'opacity 15ms cubic-bezier(0.4, 0, 0.2, 1)'
-      },
-      ':disabled': {
-        color: 'var(--on-surface)',
-        '::after': {
-          opacity: 'var(--divider)' as any
-        }
-      }
-    },
-    contained: {
-      '--multiplier': '2',
-      '--overlay-color': 'var(--on-surface)',
-      backgroundColor: 'var(--bg-color, #808080)',
-      color: 'var(--text-color)',
-      // outline
-      '::after': {
-        content: '""',
-        boxSizing: 'border-box',
-        pointerEvents: 'none',
-        position: 'absolute',
-        top: '0',
-        left: '0',
-        width: '100%',
-        height: '100%',
-        border: '2px solid var(--on-surface)',
-        borderRadius: 'inherit',
-        opacity: 0,
-        transition: 'opacity 15ms cubic-bezier(0.4, 0, 0.2, 1)'
-      },
-      ':focus': {
-        '::after': {
-          opacity: 1
-        }
-      },
-      ':disabled': {
-        backgroundColor: 'transparent',
-        color: 'var(--on-surface)',
-        '::before': {
-          opacity: 'var(--divider)' as any
-        }
-      }
+    button: {
+      fontFamily: 'sans',
+      fontSize: 'body2',
+      fontWeight: 500,
+      lineHeight: 1.7143,
+      letterSpacing: '0.02857em'
     }
   }
 }
