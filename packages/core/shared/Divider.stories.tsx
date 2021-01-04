9183d4715410dfd5879c6effc020abcf87224b60
8th713 <8th713@gmail.com>
2021-01-04 22:26:34 +0900

    add Divider component

---
+++ packages/core/shared/Divider.stories.tsx
@@ -0,0 +1,60 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+import { Box, Flex } from './Box'
+import { Divider, DividerProps } from './Divider'
+
+export default {
+  title: 'Components/Divider',
+  component: Divider,
+} as Meta
+
+const Template: Story<DividerProps> = (args) => <Divider {...args} />
+
+export const WithBox = Template.bind({})
+WithBox.decorators = [
+  (Story) => (
+    <Box css={{ backgroundColor: '$surface' }}>
+      <Box css={{ height: '$smH' }} />
+      <Story />
+      <Box css={{ height: '$smH' }} />
+    </Box>
+  ),
+]
+
+export const Inset = Template.bind({})
+Inset.args = {
+  css: { marginX: '$3' },
+}
+Inset.decorators = WithBox.decorators
+
+export const WithFlexRow = Template.bind({})
+WithFlexRow.decorators = [
+  (Story) => (
+    <Flex
+      css={{
+        flexDirection: 'row',
+        backgroundColor: '$surface',
+      }}
+    >
+      <Box css={{ flexGrow: 1, height: '$smH' }} />
+      <Story />
+      <Box css={{ flexGrow: 1, height: '$smH' }} />
+    </Flex>
+  ),
+]
+
+export const WithFlexColumn = Template.bind({})
+WithFlexColumn.decorators = [
+  (Story) => (
+    <Flex
+      css={{
+        flexDirection: 'column',
+        backgroundColor: '$surface',
+      }}
+    >
+      <Box css={{ height: '$smH' }} />
+      <Story />
+      <Box css={{ height: '$smH' }} />
+    </Flex>
+  ),
+]
---
+++ packages/core/shared/Divider.tsx
@@ -0,0 +1,22 @@
+import { StitchesProps, styled } from '../stitches.config'
+
+export type DividerProps = StitchesProps<typeof Divider>
+
+export const Divider = styled('div', {
+  boxSizing: 'border-box',
+  width: 'initial',
+  height: 'initial',
+  minHeight: 1,
+  minWidth: 1,
+  flexGrow: 0,
+  flexShrink: 0,
+  margin: 0,
+  padding: 0,
+  borderWidth: 0,
+  backgroundColor: '$onSurface',
+  opacity: 0.12,
+})
+
+if (__DEV__) {
+  Divider.displayName = 'Divider'
+}
