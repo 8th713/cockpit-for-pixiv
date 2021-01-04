03e2f8ebcd3c07c027eb45864cfd12e66ab097dc
8th713 <8th713@gmail.com>
2021-01-04 22:21:12 +0900

    add other storybooks

---
+++ packages/core/shared/Flex.stories.tsx
@@ -0,0 +1,45 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+import { Box, Flex, FlexProps } from './Box'
+
+export default {
+  title: 'Components/Flex',
+  component: Flex,
+} as Meta
+
+const Template: Story<FlexProps> = (args) => <Flex {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  children: 'Text',
+}
+
+export const Nest = Template.bind({})
+Nest.args = {
+  css: {
+    gap: '$2',
+  },
+  children: (
+    <>
+      <Box
+        css={{
+          padding: '$2',
+          backgroundColor: '$primary',
+          color: '$onPrimary',
+          flexGrow: 1,
+        }}
+      >
+        Primary
+      </Box>
+      <Box
+        css={{
+          padding: '$2',
+          backgroundColor: '$secondary',
+          color: '$onSecondary',
+        }}
+      >
+        Secondary
+      </Box>
+    </>
+  ),
+}
