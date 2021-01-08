4956061e1eacdd33acd8cdbef4b6a0b5d761c7b6
8th713 <8th713@gmail.com>
2021-01-08 23:55:50 +0900

    add banner

---
+++ packages/core/banner.js
@@ -0,0 +1,37 @@
+/*
+// ==UserScript==
+// @name         cockpit for pixiv
+// @description  Provide comfortable pixiv browsing.
+// @version      <%= version %>
+// @author       <%= author %>
+// @homepage     <%= homepage %>
+// @supportURL   <%= bugs.url %>
+// @license      <%= license %>
+// @namespace    http://github.com/8th713
+// @match        https://www.pixiv.net/*
+// @exclude      https://www.pixiv.net/novel/*
+// @grant        none
+// ==/UserScript==
+*/
+
+// prettier-ignore
+const CP_SELECTORS = {
+  /**! ビューで開く対象となる要素のセレクタ */
+  INCLUDES: [
+    'a[href*="/artworks/"]',
+    'a[href*="member_illust.php"][href*="mode=medium"][href*="illust_id="]'
+  ],
+  /**! 誤動作する要素のセレクタ */
+  EXCLUDES: [
+    '._one-click-bookmark',
+    '.thumbnail-menu',
+    '.thumbnail-menu *',
+    '._history-item.show-detail'
+  ]
+  /**! セミコロンを削除しないでください！！ */
+};
+/**!
+ * =============================
+ * ここから下は編集禁止
+ * =============================
+ */
