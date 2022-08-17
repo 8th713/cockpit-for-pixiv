834607ecec7524a88038981d32f78102d01d3e1e
8th713 <8th713@gmail.com>
2022-08-17 11:42:26 +0900

    Updates

--- analytics.js
+++
@@ -1,1 +0,0 @@
-!function(e,a,t,n,g,c,o){e.GoogleAnalyticsObject=g,e.ga=e.ga||function(){(e.ga.q=e.ga.q||[]).push(arguments)},e.ga.l=1*new Date,c=a.createElement(t),o=a.getElementsByTagName(t)[0],c.async=1,c.src="https://www.google-analytics.com/analytics.js",o.parentNode.insertBefore(c,o)}(window,document,"script",0,"ga"),ga("create","UA-33220357-5","auto"),ga("send","pageview");
